ee23b1582c4e4ad7c50dcd012378842038fd42ac
ADENCASTRO <adiddle99@gmail.com>
2024-05-25 15:20:05 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,9 @@
-console.log ('Hello World!');
-let player = 'X';
+const idInput = document.querySelector('#idInput');
+const colorInput = document.querySelector('#colorInput');
 
-function play(evt) {
-  const targetSquare = evt.target;
-  targetSquare.innerText = player;
-
-  if (player === 'X') {
-    player = 'O';
-  } else {
-    player = 'X';
-  }
-
-  const playerSpan = document.querySelector('#current-player');
-  playerSpan.innerText = player;
-
-  const winner = calculateWinner();
-  if (winner) {
-    alert(`${winner} is the winner!`);
-  } else if (isBoardFull()) {
-    alert('Game is a tie!');
-  }
+function setCard() {
+  const card = document.querySelector(`#${idInput.value}`);
+  card.style.color = colorInput.value;
 }
 
-const squares = document.querySelectorAll('.square');
-
-for (const square of squares) {
-  square.addEventListener('click', play);
-}
-
-function calculateWinner() {
-  const lines = [
-    
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (const line of lines) {
-    const [a, b, c] = line;
-    const squareAText = squares[a].innerText;
-    const squareBText = squares[b].innerText;
-    const squareCText = squares[c].innerText;
-    if (squareAText !== '' && squareAText === squareBText && squareAText === squareCText) {
-      return squareAText;
-    }
-  }
-
-  return undefined;
-}
-
-function isBoardFull() {
-  for (const square of squares) {
-    if (square.innerText === '') {
-      return false;
-    }
-  }
-  return true;
-}
+document.querySelector('#editCardButton').addEventListener('click', setCard);
